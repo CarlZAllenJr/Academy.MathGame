8307aeef04d533dc3ca74213037c6bcff87a6f25
DevZeb <carlzallenjr@gmail.com>
2024-05-10 10:39:02 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Let's play a math game!");
+Console.WriteLine("I will provide you with a math equation and you select the math operator that would make the equation true.");
+Console.WriteLine("When you are ready, type 'r' and hit enter");
+Console.ReadLine();
